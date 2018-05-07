c2b99aa779f04c8f0dd8d361e16e3f0994170811
Alexander Schnapp <aschnapp@e1z3r5p8.42.us.org>
2018-05-06 19:29:02 -0700

    Normed

--- src/malloc.c
+++ src/malloc.c
@@ -1,37 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   malloc.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aschnapp <aschnapp@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/05/06 19:24:24 by aschnapp          #+#    #+#             */
+/*   Updated: 2018/05/06 19:24:47 by aschnapp         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "malloc.h"
 
-void  init_global(void)
+void	init_global(void)
 {
-  g_head.ps = getpagesize();
-  g_head.init = 1;
-  g_head.m_size = getpagesize() * 32;
-  g_head.s_size = getpagesize();
-  g_head.s = NULL;
-  g_head.m = NULL;
-  g_head.l = NULL;
+	g_head.ps = getpagesize();
+	g_head.init = 1;
+	g_head.m_size = getpagesize() * 32;
+	g_head.s_size = getpagesize();
+	g_head.s = NULL;
+	g_head.m = NULL;
+	g_head.l = NULL;
 }
 
-t_heap **find_size(size_t size)
+t_heap	**find_size(size_t size)
 {
-  IFRET(size <= g_head.s_size, &(g_head.s));
-  IFRET(size <= g_head.m_size, &(g_head.m));
-  return &(g_head.l);
+	IFRET(size <= g_head.s_size, &(g_head.s));
+	IFRET(size <= g_head.m_size, &(g_head.m));
+	return (&(g_head.l));
 }
 
-void  *malloc(size_t size)
+void	*malloc(size_t size)
 {
-  t_heap **heap;
-  t_heap *tmp;
-  
-  IFRET(size > INT_MAX || size < 0, NULL);
-  if (!g_head.init)
-    init_global();
-  heap = find_size(size + sizeof(t_block));
-  if ((*heap) == NULL || size > g_head.m_size) 
-  {
-    IFRET(create_add_heap_to_head(size, heap), NULL);
-    return (void *)(((char *)(*heap + 1)) + sizeof(t_block));
-  }
-  IFRET(!(tmp = traverse_heap(size, heap)), NULL);
-  return (void *)(tmp->head + 1);
+	t_heap **heap;
+	t_heap *tmp;
+
+	IFRET(size > INT_MAX || size < 0, NULL);
+	if (!g_head.init)
+		init_global();
+	heap = find_size(size + sizeof(t_block));
+	if ((*heap) == NULL || size > g_head.m_size)
+	{
+		IFRET(create_add_heap_to_head(size, heap), NULL);
+		return ((void *)(((char *)(*heap + 1)) + sizeof(t_block)));
+	}
+	IFRET(!(tmp = traverse_heap(size, heap)), NULL);
+	return ((void *)(tmp->head + 1));
 }
